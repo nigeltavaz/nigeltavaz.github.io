6e86da1c9f0e3d3faf712e283613af90ea1bf1d7
nigeltavaz <164061111+nigeltavaz@users.noreply.github.com>
2024-04-04 17:57:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+
